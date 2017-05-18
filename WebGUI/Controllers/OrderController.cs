5cd430d4f244b40e913207999ff8072c03ff26d5
CodingDK <CodingDK@users.noreply.github.com>
2017-05-18 10:37:21 +0200

    scaffolding views

---
+++ WebGUI/Controllers/OrderController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using WebGUI.Models1;
+
+namespace WebGUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        private WebGUIContext db = new WebGUIContext();
+
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Date")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Date")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebGUI/Models/OrderLineController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using WebGUI.Models1;
+
+namespace WebGUI.Models
+{
+    public class OrderLineController : Controller
+    {
+        private WebGUIContext db = new WebGUIContext();
+
+        // GET: OrderLine
+        public ActionResult Index()
+        {
+            return View(db.OrderLines.ToList());
+        }
+
+        // GET: OrderLine/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderLine orderLine = db.OrderLines.Find(id);
+            if (orderLine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderLine);
+        }
+
+        // GET: OrderLine/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderLine/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Quantity,Price")] OrderLine orderLine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OrderLines.Add(orderLine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orderLine);
+        }
+
+        // GET: OrderLine/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderLine orderLine = db.OrderLines.Find(id);
+            if (orderLine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderLine);
+        }
+
+        // POST: OrderLine/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Quantity,Price")] OrderLine orderLine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderLine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orderLine);
+        }
+
+        // GET: OrderLine/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderLine orderLine = db.OrderLines.Find(id);
+            if (orderLine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderLine);
+        }
+
+        // POST: OrderLine/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            OrderLine orderLine = db.OrderLines.Find(id);
+            db.OrderLines.Remove(orderLine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
