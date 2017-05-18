7a1cc6a01ba34e0cc27577edc79e8adddce71559
CodingDK <CodingDK@users.noreply.github.com>
2017-05-18 08:52:35 +0200

    Added Model layer and classes

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models {
+    class Order {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public List<OrderLine> Lines { get; set; }
+
+    }
+}
---
+++ Models/OrderLine.cs
@@ -0,0 +1,8 @@
+﻿namespace Models {
+    public class OrderLine {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
